e32e983e1c58d9f3ea4726ebdc10c385eb07efdd
DanielDiezMiguel <daniel.diez@bosonit.com>
2022-09-20 10:21:24 +0200

    Initial structure

---
+++ Secondary/src/main/java/com/bosonit/Secundario/SecundarioApplication.java
@@ -0,0 +1,13 @@
+package com.bosonit.Secundario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecundarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecundarioApplication.class, args);
+	}
+
+}
---
+++ Secondary/src/test/java/com/bosonit/Secundario/SecundarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.Secundario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecundarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
